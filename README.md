6399d8cd29a049f37093d1ffbc76f499e2e970ad
DanielReverter <daniel.reverter.condal@gail.com>
2022-08-01 17:07:28 +0200

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# project-III-geospatial-data
+# W5 Project - Geoqueries
+​
+## Premise
+​
+The following task has been assigned to me:
+
+You recently created a new company in the GAMING industry. The company will have the following scheme:
+
+- 20 Designers
+- 5 UI/UX Engineers
+- 10 Frontend Developers
+- 15 Data Engineers
+- 5 Backend Developers
+- 20 Account Managers
+- 1 Maintenance guy that loves basketball
+- 10 Executives
+- 1 CEO/President.
+As a data engineer you have asked all the employees to show their preferences on where to place the new office. Your goal is to place the new company offices in the best place for the company to grow. You have to find a place that more or less covers all the following requirements (note that it's impossible to cover all requirements, so you have to prioritize at your glance):
+
+- Designers like to go to design talks and share knowledge. There must be some nearby companies that also do design.
+- 30% of the company staff have at least 1 child.
+- Developers like to be near successful tech startups that have raised at least 1 Million dollars.
+- Executives like Starbucks A LOT. Ensure there's a starbucks not too far.
+- Account managers need to travel a lot.
+- Everyone in the company is between 25 and 40, give them some place to go party.
+- The CEO is vegan.
+- If you want to make the maintenance guy happy, a basketball stadium must be around 10 Km.
+- The office dog—"Dobby" needs a hairdresser every month. Ensure there's one not too far away.
+​
+## Approach
+​
+From a dataset of 18000 companies around the world and all of their offices I will try to pick the one that better fulfills my requirements. For simplicity I have picked the subset of only the offices located in Barcelona, Madrid and Valencia, but the code is scalable to pick the best venue in the whole world.
+
+The criteria for which venue is the best is based on distance to the places that fulfill each of the requirements, how many employees are affected by them, the importance of an affected employee to the company and the relative importace of each requirement to an employee. The details are written in the notebook.
+
+At the end of the notebbok there are three maps, one for the best venue in each city and all of the surrounding nearest places that fulfill the requirements.
+​
+## Project files
+​
+The main directory has 2 subdirectories:
+
+* Output: Contains files created from the original data (enriched and cleaned dataframes, plots, etc.) that are used multiple times through the project.
+* src: Contains python files with all functions created specifically for this analysis.
+​
+In the root directory there is a Jupyter Notebook file that includes all the code used in the project, explaining the process at the same time
--- src/functions.py
+++ src/functions.py
@@ -19,6 +19,12 @@ load_dotenv()
 key = os.getenv("KEY")
 
 def spain_offices():
+    '''
+    This function creates a dataframe of all the offices in our mongoDB dataset that are in Madrid, Barcelona and Valencia.
+    The columns of the resulting dataframe are: 
+    The name of the company owning the office, the city it's in, office_number(in case one company has more than 2 offices in the same city),
+    the adress, latitude and longitude.
+    '''
     client = MongoClient("localhost:27017")
     db = client.get_database("Ironhack")    
     companies = db.get_collection("companies")
@@ -50,3 +56,131 @@ def spain_offices():
     spain_dict = {"name":name, "city":city, "office_number":office_number, "address":address, "latitude":latitude, "longitude":longitude}
     df = pd.DataFrame.from_dict(spain_dict)
     return df
+
+def get_coordinates (place):
+    '''
+    A function that gets the latitude and longitude of a place using geocode app. If it doesn't find it, [NaN, NaN] is returned
+    '''
+    try:
+        res = requests.get(f"https://geocode.xyz/{place}?json=1").json()
+        return [res["longt"],res["latt"]]
+    except:
+        return [np.nan, np.nan]
+
+def clean_offices(df):
+    '''
+    A function that cleans a dataframe. It drops any rows containing NaN in address, latitude and longitude at the same time,
+    then if there are some rows that have address but no lat-lon, it tries to fill those using the geocode function.
+    Finally, it drops any rows with missing lat-lon or lat-lons not in Spain
+    '''
+    df['address'].replace('', np.nan, inplace=True)
+    df = df.dropna(subset=["address", "latitude", "longitude"], how='all')
+    for i in df.index.values.tolist():
+        if math.isnan(df.at[i, "latitude"]) or df.at[i, "latitude"]==0:
+            address_complete = df.at[i, "address"] + ", " + df.at[i, "city"] + ", Spain"
+            coordinates = get_coordinates(address_complete)
+            df.at[i, "latitude"]= float(coordinates[1])
+            df.at[i, "longitude"]= float(coordinates[0])
+    for i in df.index.values.tolist():
+        if df.at[i, "latitude"] < 20:
+            latitude = df.at[i, "latitude"]
+            longitude = df.at[i, "longitude"]
+            df.at[i, "latitude"] = longitude    
+            df.at[i, "longitude"] = latitude
+    df['latitude'] = df['latitude'].fillna(0)
+    df.drop(df[df.latitude < 30].index, inplace=True)
+    df.drop(df[df.longitude < -10].index, inplace=True)
+    return df
+
+def company_positions():
+    '''
+    Function that creates a dataframe of all the positions in the company, their salaries and how many employees in each position
+    '''
+    position = ["Designers", "UI/UX Engineers", "Frontend Developers", "Data Engineers", 
+             "Backend Developers", "Account Managers", "Maintenance", "Executives", "CEO"]
+    number_of_employees = [20, 5, 10, 15, 5, 20, 1, 10, 1]
+    salary = [30000, 33000, 33000, 44000, 39000, 25000, 20000, 60000, 80000]
+    employees_dict = {"position":position, "number_of_employees":number_of_employees, "salary":salary}
+    return pd.DataFrame.from_dict(employees_dict)
+
+def requirements(df2):
+    '''
+    Function that creates a dataframe with every requirement and its relaive weight
+    '''
+    requirements = ["Design company", "Schools", "Successful tech startup", "Starbucks", "Airport", "Party", 
+                "Vegan restaurant", "Basketball stadium", "Dog hairdresser"]
+    related_employees = ["Designers", "30% of All", "Frontend Developers/Backend Developers", "Executives", "Account Managers", "All", 
+                        "CEO", "Maintenance", "All"]
+    weight = [80, 100, 80, 40, 100, 10, 300, 500, 1]
+    requirements_dict = {"requirements":requirements, "related_employees":related_employees, "weight":weight}
+    df3 = pd.DataFrame.from_dict(requirements_dict)
+    final_weight = []
+    everyone = list(df2["position"].values)
+    related_employees = [["Designers"], everyone, ['Frontend Developers', 'Backend Developers'], ['Executives'], ['Account Managers'], everyone, ['CEO'], ['Maintenance'], everyone] 
+    for i, employee_types in enumerate(related_employees):
+        weight = df3.at[i, "weight"]
+        final = 0
+        for employee_type in employee_types:
+            number_of_employees = list(df2.loc[df2['position'] == employee_type]["number_of_employees"].values)[0]
+            salary = list(df2.loc[df2['position'] == employee_type]["salary"].values)[0]
+            final += weight*number_of_employees*salary
+        if i == 1: 
+            final *= 0.3
+        final_weight.append(int(final))
+    df3['final_weight'] = final_weight
+    return df3
+
+def get_results_from_foursquare(query, location, limit):
+    '''
+    This function gets query results from foursquare
+    '''
+    url = f"https://api.foursquare.com/v3/places/search?query={query}&ll={location[0]}%2C{location[1]}&radius=100000&sort=RELEVANCE&limit={limit}"
+    headers = {
+        "Accept": "application/json",
+        "Authorization": key
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()["results"]
+
+def get_distance_column(query, df):
+    '''
+    Function used to add a column as described in the notebook
+    '''
+    limit = 10
+    distance_list = []
+    for index in df.index.values.tolist():
+        location = [df.at[index, "latitude"], df.at[index, "longitude"]]
+        results = get_results_from_foursquare (query, location, limit)
+        temp_distances = []
+        for result in results:
+            temp_distances.append(result["distance"])
+        if len(temp_distances)>0:
+            distance_list.append(min(temp_distances))
+        else:
+            distance_list.append(100000)
+    return distance_list
+
+def ordered_df(df):
+    '''
+    Function that reorders the requirements dataframe column as described in the notebook
+    '''
+    cols = df.columns.tolist()
+    cols2 = cols[0:6] + [cols[-2]] + [cols[6]] + [cols[-1]] + cols[7:9] + [cols[-4]] + cols[9:11] + [cols[-3]]
+    return df[cols2]
+
+def sum_column(df, df3):
+    '''
+    Function that adds a final column to the requirements df used to determine the best venue as described in the notebook
+    '''
+    sum_of_distances = []
+    list_of_columns_to_add = list(df.columns)[6:]
+    for index in df.index.values.tolist():
+        sum_ = 0
+        i = 0
+        for column in list_of_columns_to_add:
+            weight = df3.at[i, "final_weight"]
+            sum_ += df.at[index, column]*weight
+            i += 1
+        sum_of_distances.append(sum_)
+    return sum_of_distances
+
